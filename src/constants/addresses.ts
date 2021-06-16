ac8bb1017c4f5c7a870a3fd768e5e4fd67535328
Harris Levine <acyclicgraphs@gmail.com>
2021-06-16 05:41:52 -0400

    Update addresses.ts

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -3,5 +3,5 @@ import { Networks } from './networks';
 export const ZORA_MEDIA_CONTRACT_BY_NETWORK = {
   // TODO: change mainnet address
   [Networks.MAINNET]: '0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7',
-  [Networks.RINKEBY]: '0x297B665B7678a7185Eaff118eA8F02fA653f12e2',
+  [Networks.RINKEBY]: '0x79b1E060c796e827c2322e3E1a73Fb06A7a132Eb',
 };
